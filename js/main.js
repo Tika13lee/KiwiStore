2772d0b2f30d9d3c866a7bb997f2e513f0c683ba
DangThiNhuQuynh19 <dangnhuquynh.ks2003@gmail.com>
2023-05-05 11:30:27 +0700

    js kiem tra thong tin nhap dang ky(trang cua hang)

--- js/main.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-    let name = document.getElementById(fullname).value;
-    let text;
-    if(name === ""){
-        text = "Vui long dien ten";
-        document.getElementById("demo").innerHTML = text;
-    }
-}
