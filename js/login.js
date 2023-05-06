3c9a93919ddd066a0bf69f4d3f0115fa8a217fa2
DangThiNhuQuynh19 <dangnhuquynh.ks2003@gmail.com>
2023-05-06 14:06:17 +0700

    edit

--- js/login.js
+++ js/login.js
@@ -5,14 +5,14 @@ function ketqua(){
     var user = localStorage.getItem(uname);
     var data = JSON.parse(user);
     if(data == null){
-        alert("Username không tồn tại")
+        alert("Tài khoản không tồn tại")
     }
-    else if(uname ==data.uname && mk ==data.mk){
+    else if(uname == data.uname && mk == data.mk){
         var json = JSON.stringify(uname);
         localStorage.setItem('tam',json);
         window.location.href="TrangChu.html"
     }
-    else if(uname !=data.uname || mk !=data.mk){
+    else if(uname != data.uname || mk != data.mk){
         alert("Tên hoặc mật khẩu không chính xác")
     }
 }
