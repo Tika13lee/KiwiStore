8363a9e2c93dd4a6acf96bb223d706ed4c83c75d
DangThiNhuQuynh19 <dangnhuquynh.ks2003@gmail.com>
2023-05-06 13:43:11 +0700

    js kiem tra dang nhap (trang chu)

---
+++ js/login.js
@@ -0,0 +1,18 @@
+function ketqua(){
+    event.preventDefault();
+    var uname = document.getElementById("txtUser").value;
+    var mk = document.getElementById("pwd").value; 
+    var user = localStorage.getItem(uname);
+    var data = JSON.parse(user);
+    if(data == null){
+        alert("Username không tồn tại")
+    }
+    else if(uname ==data.uname && mk ==data.mk){
+        var json = JSON.stringify(uname);
+        localStorage.setItem('tam',json);
+        window.location.href="TrangChu.html"
+    }
+    else if(uname !=data.uname || mk !=data.mk){
+        alert("Tên hoặc mật khẩu không chính xác")
+    }
+}
