d4ee0116e7c0abaa4e01f90f4aaf6db6e4ffd02c
Thuy Kieu <thuykieu130303@gmail.com>
2023-05-06 10:09:00 +0700

    login

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-function myFunction(){
-    let name = document.getElementById(fullname).value;
-    let text;
-    if(name === ""){
-        text = "Vui long dien ten";
-        document.getElementById("demo").innerHTML = text;
+var cart = [];
+
+var addToCartButton = document.querySelector('.btnAdd');
+
+addToCartButton.addEventListener('click', event => {
+    const productId = new URLSearchParams(window.location.search).get('id');
+    const product = products.find(product => product.id === parseInt(productId));
+
+    if (product) {
+        // Thêm sản phẩm vào giỏ hàng
+        cart.push({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            quantity: 1
+        });
+
+        // Thông báo cho người dùng
+        alert('Thêm sản phẩm vào giỏ hàng thành công!');
     }
-}
+});
